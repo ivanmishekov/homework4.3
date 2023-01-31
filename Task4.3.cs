68e1339d33876d9453173fd5f456664cc23d3384
Ivan <mishekov6868@gmail.com>
2023-01-31 19:53:23 +0300

    1

---
+++ Task4.3.cs
@@ -0,0 +1,33 @@
+﻿/*
+Задача 3: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Сделать через функции
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[,] matrix = new int[1, 8];
+FillArray(matrix);
+PrintArray(matrix);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}, ");
+        }        
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,100);
+        }
+    }
+}
